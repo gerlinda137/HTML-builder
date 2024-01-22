bce0abcc60f69a1b5e73d1311df872071a290b5e
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-01-22 22:44:00 +0400

    feat: second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+
+stdout.write('Write down some of your current thoughts\n');
+process.on('SIGINT', () => {
+  stdout.write(
+    'Thank you, all of your thoughts will be transfered to Tovarisch Mayor \n',
+  );
+  process.exit();
+});
+
+fs.writeFile(path.join(__dirname, 'text.txt'), '', (err) => {
+  if (err) throw err;
+});
+stdin.on('data', (data) => {
+  const dataString = data.toString();
+  if (dataString == 'exit\n') {
+    stdout.write(
+      'Thank you, all of your thoughts will be transfered to Tovarisch Mayor \n',
+    );
+    process.exit();
+  }
+  fs.appendFile(path.join(__dirname, 'text.txt'), data, (err) => {
+    if (err) throw err;
+    console.log('Anything else?');
+  });
+});
