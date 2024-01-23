72f981e21ec143853aa231ed6e047d47c49b793d
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-01-23 19:16:54 +0400

    feat: finish task five

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,67 +6,23 @@ fs.readdir(
   { withFileTypes: true },
   (err, data) => {
     if (err) throw err;
-    fs.stat(
+    const bundle = fs.createWriteStream(
       path.join(__dirname, 'project-dist', 'bundle.css'),
-      function (err, stat) {
-        if (err == null) {
-          for (const iterator of data) {
-            if (iterator.isFile()) {
-              const fileName = iterator.name;
-              let extName = path.extname(fileName);
-              extName = extName.substring(1);
-              if (extName == 'css') {
-                fs.readFile(
-                  path.join(__dirname, 'styles', fileName),
-                  'utf-8',
-                  (err, fileContent) => {
-                    if (err) throw err;
-                    fs.appendFile(
-                      path.join(__dirname, 'project-dist', 'bundle.css'),
-                      fileContent,
-                      (err) => {
-                        if (err) throw err;
-                      },
-                    );
-                  },
-                );
-              }
-            }
-          }
-        } else if (err.code === 'ENOENT') {
-          // file does not exist
-          fs.writeFile(
-            path.join(__dirname, 'project-dist', 'bundle.css'),
-            '',
-            (err) => {
-              if (err) throw err;
-              for (const iterator of data) {
-                if (iterator.isFile()) {
-                  const fileName = iterator.name;
-                  let extName = path.extname(fileName);
-                  extName = extName.substring(1);
-                  if (extName == 'css') {
-                    fs.readFile(
-                      path.join(__dirname, 'styles', fileName),
-                      'utf-8',
-                      (err, fileContent) => {
-                        if (err) throw err;
-                        fs.appendFile(
-                          path.join(__dirname, 'project-dist', 'bundle.css'),
-                          fileContent,
-                          (err) => {
-                            if (err) throw err;
-                          },
-                        );
-                      },
-                    );
-                  }
-                }
-              }
-            },
-          );
-        }
-      },
     );
+
+    for (const iterator of data) {
+      if (iterator.isFile()) {
+        const fileName = iterator.name;
+        let extName = path.extname(fileName);
+        extName = extName.substring(1);
+        if (extName == 'css') {
+          const input = fs.createReadStream(
+            path.join(__dirname, 'styles', fileName),
+            'utf-8',
+          );
+          input.on('data', (chunk) => bundle.write(chunk));
+        }
+      }
+    }
   },
 );
