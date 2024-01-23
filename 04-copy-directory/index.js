571b12e3a86aa3f41d5abc1f899a5badcfe0c1b0
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-01-23 21:55:05 +0400

    fix: fix copy files when change them

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,25 +3,28 @@ const path = require('path');
 
 fs.readdir(path.join(__dirname), (err, data) => {
   if (err) throw err;
-  let hasDir = false;
   for (const iterator of data) {
     if (iterator == 'files-copy') {
       hasDir = true;
     }
   }
-  if (!hasDir) {
-    fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
-      if (err) throw err;
-    });
-  }
-  fs.readdir(path.join(__dirname, 'files'), (err, data) => {
-    if (err) throw err;
-    for (const iterator of data) {
-      const origPath = path.join(__dirname, 'files', iterator);
-      const targetPath = path.join(__dirname, 'files-copy', iterator);
-      fs.copyFile(origPath, targetPath, (err) => {
+  fs.rm(
+    path.join(__dirname, 'files-copy'),
+    { recursive: true, force: true },
+    () => {
+      fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
         if (err) throw err;
       });
-    }
-  });
+      fs.readdir(path.join(__dirname, 'files'), (err, data) => {
+        if (err) throw err;
+        for (const iterator of data) {
+          const origPath = path.join(__dirname, 'files', iterator);
+          const targetPath = path.join(__dirname, 'files-copy', iterator);
+          fs.copyFile(origPath, targetPath, (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+    },
+  );
 });
