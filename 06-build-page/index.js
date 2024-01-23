8471d0deff6262aa2c54981e4058908416649956
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-01-24 02:11:15 +0400

    feat: finish sixth task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -23,21 +23,44 @@ fs.readdir(path.join(__dirname), (err, data) => {
     path.join(__dirname, 'template.html'),
     'utf-8',
   );
-  templateStream.on('data', (chunk) => {
-    console.log(chunk);
-  });
-
-  // fs.readdir(path.join(__dirname, 'files'), (err, data) => {
-  //   if (err) throw err;
-  //   for (const iterator of data) {
-  //     const origPath = path.join(__dirname, 'files', iterator);
-  //     const targetPath = path.join(__dirname, 'files-copy', iterator);
-  //     fs.copyFile(origPath, targetPath, (err) => {
-  //       if (err) throw err;
-  //     });
-  //   }
-  // });
+  templateStream.on('data', readComponents);
 });
+
+function readComponents(mainTemplate) {
+  fs.readdir(path.join(__dirname, 'components'), (err, components) => {
+    if (err) throw err;
+    const componentsPairs = new Map();
+    let counter = 0;
+    for (const iterator of components) {
+      counter++;
+      const componentStream = fs.createReadStream(
+        path.join(__dirname, 'components', iterator),
+        'utf-8',
+      );
+      componentStream.on('data', (chunk) => {
+        const componentContent = chunk;
+        const componentVariable = `{{${iterator.replace('.html', '')}}}`;
+        componentsPairs.set(componentVariable, componentContent);
+        counter--;
+        if (counter == 0) {
+          printHtml(componentsPairs, mainTemplate);
+        }
+      });
+    }
+  });
+}
+
+function printHtml(map, mainTemplate) {
+  let replacedHtml = mainTemplate;
+  for (const [key, value] of map) {
+    replacedHtml = replacedHtml.replace(key, value);
+  }
+  const finalHtmlStream = fs.createWriteStream(
+    path.join(__dirname, 'project-dist', 'index.html'),
+  );
+
+  finalHtmlStream.write(replacedHtml);
+}
 
 // styles
 fs.readdir(
@@ -46,7 +69,7 @@ fs.readdir(
   (err, data) => {
     if (err) throw err;
     const bundle = fs.createWriteStream(
-      path.join(__dirname, 'project-dist', 'bundle.css'),
+      path.join(__dirname, 'project-dist', 'style.css'),
     );
 
     for (const iterator of data) {
@@ -76,6 +99,7 @@ fs.readdir(path.join(__dirname), (err, data) => {
     copyDir(pathOrigDir, pathTargetDir);
   });
 });
+
 function copyDir(dirPath, targetDirPath) {
   fs.mkdir(targetDirPath, (err) => {
     if (err) throw err;
